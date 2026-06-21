a334827727109c2e84b93691b6906bad1fe7163c
NikolayGrozev <nikigr2009@abv.bg>
2026-06-22 02:04:22 +0300

    added feature - can select a file from the console

--- cardGame.c
+++ cardGame.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct Card {
     char power;
@@ -27,13 +28,60 @@ int decrypt_single_card(FILE *file, const char *key, int cardIndex, Card *drawnC
     return 0;
 }
 
+char * InputFileName() {
+    int size = 4;
+    char * c = (char *)calloc(size, sizeof(char));
+    
+    if (c == NULL) {
+        printf("\nError in allocating memory");
+        exit(1);
+    }
+
+    printf("\nEnter file name: ");
+
+    int count = 0;
+    int ch; 
+
+    while ((ch = getchar()) != '\n' && ch != EOF) {
+        
+        if (count == size - 1) {
+            size *= 2;
+            
+            char * temp = (char *)realloc(c, size * sizeof(char));
+            if (temp == NULL) {
+                free(c);
+                printf("\nError in reallocating memory");
+                exit(1);
+            }
+            c = temp;
+        }
+        
+        c[count++] = (char)ch;
+    }
+
+    c[count] = '\0';
+
+    c = (char *)realloc(c, (count + 1) * sizeof(char));
+    if (c == NULL) {
+        printf("\nError in reallocating memory");
+        exit(1);
+    }
+
+    return c;
+}
+
 int main()
 {
-    FILE *file = fopen("deck.txt", "r");
+    char * filename = InputFileName();
+
+    FILE *file = fopen(filename, "r");
+    free(filename);
     if (!file) {
         printf("Error opening file %s\n", file);
         return 0;
     }
+
+    printf("\nOpened File sucessfully ");
 
     Card memory_array[52];
     int count = 0;
